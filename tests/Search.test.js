6eda9b0c60f51d6ba36387ebb338fd5d6a3c1581
Timofey Bychkov <boobooin@gmail.com>
2026-09-25 19:46:28 +0100

    test: закрепить отсутствие эха в Search под v-model родителя

--- tests/Search.test.js
+++ tests/Search.test.js
@@ -1,6 +1,7 @@
 // @vitest-environment happy-dom
 import { afterEach, describe, expect, it } from 'vitest'
 import { enableAutoUnmount, mount } from '@vue/test-utils'
+import { h } from 'vue'
 import Search from '../src/components/Search.vue'
 
 enableAutoUnmount(afterEach)
@@ -22,5 +23,29 @@ describe('Search', () => {
         expect(wrapper.get('input').element.value).toBe('доллар')
         expect(wrapper.emitted('update:modelValue')).toBeUndefined()
         expect(wrapper.emitted('changed')).toBeUndefined()
+    })
+
+    it('при вводе под v-model родителя шлёт по одному событию без эха', async () => {
+        const Parent = {
+            data() {
+                return { query: '' }
+            },
+            render() {
+                return h(Search, {
+                    header: 'Поиск',
+                    modelValue: this.query,
+                    'onUpdate:modelValue': (value) => (this.query = value),
+                })
+            },
+        }
+        const wrapper = mount(Parent)
+        const search = wrapper.findComponent(Search)
+
+        await search.get('input').setValue('евро')
+
+        expect(wrapper.vm.query).toBe('евро')
+        expect(search.get('input').element.value).toBe('евро')
+        expect(search.emitted('update:modelValue')).toEqual([['евро']])
+        expect(search.emitted('changed')).toHaveLength(1)
     })
 })
